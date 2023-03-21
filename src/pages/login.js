0f02fba92515fbca11c741fa2040e324773a8cfd
StephanieVal07 <107519654+StephanieVal07@users.noreply.github.com>
2023-03-21 19:59:26 -0400

    Updated login
    Login page can take login data from users.

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,27 +1,56 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import { Layout } from "../components/common";
 
-const LoginForm = ({ isShowLogin }) => {
+const LoginForm = () => {
+  const [loginInfo, setloginInfo] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+      setloginInfo({...loginInfo, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      
+      console.log(loginInfo);
+
+      setloginInfo({ username: "", password: ""});
+  };
+
   return (
     <Layout>
-        <div className={`${isShowLogin ? "active" : ""} show`}>
         <div className="login-form">
             <div className="form-box solid">
             <form className="App">
                 <h1 className="login-text">Login</h1>
                 <label>Username</label>
                 <br></br>
-                <input type="text" placeholder="Username or Email" name="username" className="login-box" />
+                <input 
+                  type="text" 
+                  placeholder="Enter Username" 
+                  name="username" 
+                  className="login-box" 
+                  value={loginInfo.username} 
+                  onChange={handleChange}
+                   />
                 <br></br>
                 <label>Password</label>
                 <br></br>
-                <input type="password" placeholder="Password" name="password" className="login-box" />
+                <input 
+                  type="password" 
+                  placeholder="Enter Password" 
+                  name="password" 
+                  className="login-box" 
+                  value={loginInfo.password} 
+                  onChange={handleChange}
+                />
                 <br></br>
-                <input type="submit" value="Login" className="login-btn" href="/index"/>
+                <input type="Submit" value="Login" className="login-btn" onClick={handleSubmit}/>
             </form>
             </div>
-        </div>
         </div>
     </Layout>
   );
