69b45a2f863a6ec69601e584995d8f58c428497e
cstea096 <119032926+cstea096@users.noreply.github.com>
2023-04-14 16:08:40 -0400

    server

--- api-server/db.js
+++ api-server/db.js
@@ -14,15 +14,49 @@ db.serialize(() => {
 			hashed_password BLOB,
 			salt BLOB,
 			name TEXT,
-			email TEXT UNIQUE
+			email TEXT UNIQUE,
+			account_type INTEGER NOT NULL
+		)
+	`);
+
+	db.run(`
+		CREATE TABLE IF NOT EXISTS auctions (
+			id INTEGER PRIMARY KEY,
+			title TEXT NOT NULL,
+			description TEXT,
+			price REAL,
+			brand TEXT,
+			category TEXT,
+			thumbnail TEXT,
+			end_time INTEGER NOT NULL,
+			complete INTEGER NOT NULL,
+			seller INTEGER NOT NULL
+		)
+	`);
+
+	db.run(`
+		CREATE TABLE IF NOT EXISTS images (
+			id INTEGER,
+			url TEXT,
+			PRIMARY KEY (id, url)
+		)
+	`);
+
+	db.run(`
+		CREATE TABLE IF NOT EXISTS authorization (
+			user_id INTEGER,
+			computation_id TEXT,
+			token TEXT,
+			PRIMARY KEY (user_id, computation_id)
 		)
 	`);
 
 	let salt = crypto.randomBytes(16);
-	db.run("INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)", [
+	db.run("INSERT OR IGNORE INTO users (username, hashed_password, salt, account_type) VALUES (?, ?, ?, ?)", [
 		"admin",
 		crypto.pbkdf2Sync("adminpassword", salt, 310000, 32, "sha256"),
 		salt,
+		true
 	]);
 });
 
--- api-server/routes/jiff.js
+++ api-server/routes/jiff.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 let express = require("express");
 let passport = require("passport");
 let LocalStrategy = require("passport-local");
 let crypto = require("crypto");
+let superagent = require("superagent");
 
 let db = require("../db");
 
@@ -16,6 +18,67 @@ router.post("/jiff/results", (req, res) => {
 
 router.get("/jiff/results", (req, res) => {
 	res.send(tempResults);
+});
+
+router.get("/jiff/compute/:id", (req, res) => {
+	let computationId = req.params.id;
+	let url = "http://localhost:8080/control/compute/" + computationId;
+	superagent.post(url).set("X-Api-Key", process.env.JIFF_KEY).end((err, res) => {
+		if (err) {
+			console.log(`Error while sending MPC begin ${err}`);
+			return res.send({
+				success: false,
+				message: "Error"
+			})
+		}
+	});
+	res.send();
+});
+
+router.get("/jiff/authenticate/:id", (req, res) => {
+	if (!req.user) return res.send({
+		success: false,
+		message: "Not logged in"
+	});
+
+	let computationId = req.params.id;
+
+	db.get("SELECT * FROM authorization WHERE user_id = ? AND computation_id = ?", [
+		req.user.id,
+		computationId
+	], (err, row) => {
+		if (err) {
+			console.log(err);
+			return res.send({
+				success: false,
+				message: "Error"
+			});
+		}
+
+		if (row && row.token) {
+			return res.send({
+				success: true,
+				message: {
+					token: row.token
+				}
+			});
+		}
+
+		let token = crypto.randomBytes(16).toString("hex");
+		db.run("INSERT INTO authorization VALUES (?, ?, ?)", [
+			req.user.id,
+			computationId,
+			token
+		], (err) => {
+			if (err) console.log(err);
+		});
+		res.send({
+			success: true,
+			message: {
+				token: token
+			}
+		});
+	});
 })
 
 module.exports = router;
